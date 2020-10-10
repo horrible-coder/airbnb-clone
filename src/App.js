e92bde952da921400ace51df45aeecbb16643c3a
zx012 <rrai9999999@gmail.com>
2020-10-11 01:38:53 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import Home from './Home';
+import Header from './Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Home />
+
+
+
+    </div>
+  );
+}
+
+export default App;
