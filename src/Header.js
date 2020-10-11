357db9ee37dd6358ecfc73369af04fd22cadf817
zx012 <rrai9999999@gmail.com>
2020-10-11 15:16:06 +0530

    Search bar edited

--- src/Header.js
+++ src/Header.js
@@ -10,7 +10,7 @@ function Header() {
     <div className='header'>
       <img className='header__icon' src="https://i.pinimg.com/originals/3c/bf/be/3cbfbe148597341fa56f2f87ade90956.png" alt="" />
       <div className='header__center'> 
-        <input type='text' />
+        <input type='text' placeholder='Start Your Search' />
         <SearchIcon />
       </div>
       <div className='header__right'> 
