38b1527848199aa257cb5dd4f11f50a4156743ee
zx012 <rrai9999999@gmail.com>
2020-10-11 15:20:27 +0530

    Created components folder

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import './App.css';
-import Home from './Home';
-import Header from './Header';
+import Home from './components/Home';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Home />
-
-
-
     </div>
   );
 }
